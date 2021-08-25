f5eb704f6e61f6a8109abe9218f9ab13f45745ff
DELL <DELL@DESKTOP-8PII8GT>
2021-08-25 10:50:18 +0300

    Add project files.

---
+++ ConsoleUI2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI2
+{
+    //SOLID
+    //Solid'in O'su:Open Closed Principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            Console.WriteLine("-------------------------------");
+            foreach (var product in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Dal: data access layer
+    public interface IProductDal : IEntityRepository<Product>     
+    {
+      
+    }
+}
