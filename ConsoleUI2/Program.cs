512c63f1bb7911386dd11c900821ce79625121f4
MertBaran <mert_baran_1998@hotmail.com>
2022-05-29 19:10:55 +0300

    Commit

--- ConsoleUI2/Program.cs
+++ ConsoleUI2/Program.cs
@@ -3,7 +3,7 @@ using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
-namespace MyConsoleUI
+namespace ConsoleUI
 {
     //SOLID
     //Solid'in O'su:Open Closed Principle
@@ -11,23 +11,38 @@ namespace MyConsoleUI
     {
         static void Main(string[] args)
         {
-            //  ProductTest();
+            ProductTest();
+            //   CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach(var category in categoryManager.GetAll())
+            foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
         }
 
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName);
+                foreach (var product in productManager.GetProductDetails().Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
         }
     }
 }
+
--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +10,6 @@ namespace DataAccess.Abstract
     //Dal: data access layer
     public interface IProductDal : IEntityRepository<Product>     
     {
-      
+        List<ProductDetailDto> GetProductDetails();
     }
 }
